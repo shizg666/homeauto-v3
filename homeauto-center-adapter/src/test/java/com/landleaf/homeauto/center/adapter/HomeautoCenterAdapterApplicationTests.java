31331fd54ed5f261f2ee310215d972ff1cd8427a
henry <hejin@landleaf-tech.com>
2020-06-22 10:02:25 +0800

    修改test

--- homeauto-center-adapter/src/test/java/com/landleaf/homeauto/center/adapter/HomeautoCenterAdapterApplicationTests.java
+++ homeauto-center-adapter/src/test/java/com/landleaf/homeauto/center/adapter/HomeautoCenterAdapterApplicationTests.java
@@ -1,6 +1,7 @@
 package com.landleaf.homeauto.center.adapter;
 
-import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
