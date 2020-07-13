6aa10e85ebdc66951967b2beeb8a44d8cd5c450a
喻九敏 <yujiumin@landleaf-tech.com>
2020-07-13 15:01:23 +0800

    删除冗余代码

--- homeauto-common/src/main/java/com/landleaf/homeauto/common/domain/po/device/DicPO.java
+++ homeauto-common/src/main/java/com/landleaf/homeauto/common/domain/po/device/DicPO.java
@@ -1,37 +1,32 @@
 package com.landleaf.homeauto.common.domain.po.device;
 
-import java.io.Serializable;
-import java.time.LocalDateTime;
-
-import com.baomidou.mybatisplus.annotation.*;
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableName;
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.landleaf.homeauto.common.domain.BaseEntity;
+import com.landleaf.homeauto.common.domain.BaseEntityNew;
+import com.landleaf.homeauto.common.thandler.BooleanTypeHandler;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
 
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
 /**
- * <p>
- * 
- * </p>
- *
  * @author Yujiumin
  * @since 2020-07-10
  */
 @Data
 @Accessors(chain = true)
-@ApiModel(value="TbDic对象", description="")
+@ApiModel("字典表对象")
 @TableName("tb_dic")
-public class DicPO implements Serializable {
+@EqualsAndHashCode(callSuper = true)
+public class DicPO extends BaseEntityNew implements Serializable {
 
     private static final long serialVersionUID = 1L;
-
-    @TableId(value = "id", type = IdType.INPUT)
-    @TableField(fill = FieldFill.INSERT)
-    @ApiModelProperty(value = "主键")
-    private Integer id;
 
     @TableField("dic_name")
     @ApiModelProperty(value = "字典名称")
@@ -61,18 +56,8 @@ public class DicPO implements Serializable {
     @ApiModelProperty(value = "字典排序值")
     private Integer dicOrder;
 
-    @TableField("is_enabled")
+    @TableField(value = "is_enabled", typeHandler = BooleanTypeHandler.class)
     @ApiModelProperty(value = "是否启用：0为否，1为是")
-    private String enabled;
-
-    @TableField(fill = FieldFill.INSERT)
-    @ApiModelProperty(value = "创建时间")
-    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone="CTT")
-    private LocalDateTime createTime;
-
-    @TableField(fill = FieldFill.INSERT_UPDATE)
-    @ApiModelProperty(value = "更新时间")
-    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone="CTT")
-    private LocalDateTime updateTime;
+    private Boolean enabled;
 
 }
