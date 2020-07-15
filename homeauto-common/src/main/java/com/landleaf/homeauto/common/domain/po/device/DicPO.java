db0e4f7f84327477a5dbdeb2e9bc17c23df59983
喻九敏 <yujiumin@landleaf-tech.com>
2020-07-15 09:06:56 +0800

    主键改为自增

--- homeauto-common/src/main/java/com/landleaf/homeauto/common/domain/po/device/DicPO.java
+++ homeauto-common/src/main/java/com/landleaf/homeauto/common/domain/po/device/DicPO.java
@@ -25,7 +25,7 @@ public class DicPO implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-    @TableId(value = "id", type = IdType.INPUT)
+    @TableId(value = "id", type = IdType.AUTO)
     @ApiModelProperty(value = "主键")
     private Integer id;
 
