bf9f905314e53e4ae9501af7f240d3b8eea60598
喻九敏 <yujiumin@landleaf-tech.com>
2020-07-22 15:01:01 +0800

    代码合并

--- homeauto-center-gateway/src/main/java/com/landleaf/homeauto/center/gateway/HomeautoCenterGatewayApplication.java
+++ homeauto-center-gateway/src/main/java/com/landleaf/homeauto/center/gateway/HomeautoCenterGatewayApplication.java
@@ -3,6 +3,8 @@ package com.landleaf.homeauto.center.gateway;
 import com.landleaf.homeauto.center.gateway.filter.AddTokenFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -14,7 +16,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 /**
  * @author wenyilu
  */
-@SpringBootApplication
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class, MongoAutoConfiguration.class})
 @EnableDiscoveryClient
 @EnableFeignClients
 @EnableScheduling
