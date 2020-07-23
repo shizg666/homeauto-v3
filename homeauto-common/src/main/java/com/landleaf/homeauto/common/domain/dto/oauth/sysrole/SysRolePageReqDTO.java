e382bab6b233b7892138438ac6ea6e49ff680365
温奕禄 <wenyilu@landleaf-tech.com>
2020-07-23 09:36:54 +0800

    刪除角色中多余字段

--- homeauto-common/src/main/java/com/landleaf/homeauto/common/domain/dto/oauth/sysrole/SysRolePageReqDTO.java
+++ homeauto-common/src/main/java/com/landleaf/homeauto/common/domain/dto/oauth/sysrole/SysRolePageReqDTO.java
@@ -1,9 +1,14 @@
 package com.landleaf.homeauto.common.domain.dto.oauth.sysrole;
 
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.landleaf.homeauto.common.domain.qry.BaseQry;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
+
+import java.time.LocalDateTime;
 
 /**
  * 系统账号角色列表分页请求DTO
@@ -22,5 +27,8 @@ public class SysRolePageReqDTO extends BaseQry {
     @ApiModelProperty(value = "启用标识，0：禁用，1：启用")
     private Integer statusName;
 
+    @ApiModelProperty(value = "创建时间")
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone="CTT")
+    private LocalDateTime createTime;
 
 }
