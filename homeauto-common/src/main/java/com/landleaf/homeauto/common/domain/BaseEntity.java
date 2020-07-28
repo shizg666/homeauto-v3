016f34cbdc7dacdad067b2a0afb7aef2e1ebf62c
shizg <shizengguang@landleaf-tech.com>
2020-07-28 17:41:40 +0800

    删除标志

--- homeauto-common/src/main/java/com/landleaf/homeauto/common/domain/BaseEntity.java
+++ homeauto-common/src/main/java/com/landleaf/homeauto/common/domain/BaseEntity.java
@@ -24,10 +24,10 @@ public class BaseEntity extends Model<BaseEntity> {
     @ApiModelProperty(value = "主键")
     private String id;
 
-    @TableLogic
-    @TableField(fill = FieldFill.INSERT)
-    @ApiModelProperty(value = "是否可用")
-    private Integer delFlag;
+//    @TableLogic
+//    @TableField(fill = FieldFill.INSERT)
+//    @ApiModelProperty(value = "是否可用")
+//    private Integer delFlag;
 
     @TableField(fill = FieldFill.INSERT)
     @ApiModelProperty(value = "创建时间")
