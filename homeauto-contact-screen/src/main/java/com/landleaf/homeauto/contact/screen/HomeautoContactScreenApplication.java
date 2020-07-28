d2d57d5e2e4159d404480ef37d9ecb3bb2c7f05d
wenyilu <123456>
2020-07-28 14:33:12 +0800

    规范配置

--- homeauto-contact-screen/src/main/java/com/landleaf/homeauto/contact/screen/HomeautoContactScreenApplication.java
+++ homeauto-contact-screen/src/main/java/com/landleaf/homeauto/contact/screen/HomeautoContactScreenApplication.java
@@ -2,13 +2,15 @@ package com.landleaf.homeauto.contact.screen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@SpringBootApplication
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class, MongoAutoConfiguration.class})
 @EnableDiscoveryClient
 @EnableFeignClients
 @EnableSwagger2
