840678a3735e37ada7f96cfac389a3a6ab396539
喻九敏 <yujiumin@landleaf-tech.com>
2020-08-10 17:45:06 +0800

    Websocket模块独立

---
+++ homeauto-center-websocket/src/main/java/com/landleaf/homeauto/center/websocket/HomeautoCenterWebSocketApplication.java
@@ -0,0 +1,21 @@
+package com.landleaf.homeauto.center.websocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * WebSocket服务
+ *
+ * @author Yujiumin
+ * @version 2020/8/10
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class HomeautoCenterWebSocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeautoCenterWebSocketApplication.class, args);
+    }
+
+}
