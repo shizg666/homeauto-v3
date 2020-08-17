0c15614b28fea87f14a395ab96f72197fb7ac720
喻九敏 <yujiumin@landleaf-tech.com>
2020-08-17 17:39:13 +0800

    场景数据传输对象

---
+++ homeauto-center-device/src/main/java/com/landleaf/homeauto/center/device/model/dto/SceneDTO.java
@@ -0,0 +1,19 @@
+package com.landleaf.homeauto.center.device.model.dto;
+
+import io.swagger.annotations.ApiModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author Yujiumin
+ * @version 2020/8/17
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ApiModel("场景数据传输对象")
+public class SceneDTO {
+
+}
+
