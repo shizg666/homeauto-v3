1f30aed115b45f887d08eafd9ed690da65105a3e
喻九敏 <yujiumin@landleaf-tech.com>
2020-08-20 10:37:54 +0800

    切换家庭

--- homeauto-center-device/src/main/java/com/landleaf/homeauto/center/device/service/mybatis/impl/FamilyUserServiceImpl.java
+++ homeauto-center-device/src/main/java/com/landleaf/homeauto/center/device/service/mybatis/impl/FamilyUserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.landleaf.homeauto.center.device.service.mybatis.impl;
 
+import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.landleaf.homeauto.center.device.model.domain.FamilyUserDO;
 import com.landleaf.homeauto.center.device.model.mapper.FamilyUserMapper;
@@ -16,5 +17,22 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class FamilyUserServiceImpl extends ServiceImpl<FamilyUserMapper, FamilyUserDO> implements IFamilyUserService {
+
+    @Override
+    public void checkoutFamily(String userId, String familyId) {
+        // 把当前用户的当前家庭设为常用家庭
+        UpdateWrapper<FamilyUserDO> updateWrapperForCheck = new UpdateWrapper<>();
+        updateWrapperForCheck.set("last_checked", 1);
+        updateWrapperForCheck.eq("family_id", familyId);
+        updateWrapperForCheck.eq("user_id", userId);
+        update(updateWrapperForCheck);
+
+        // 把当前用户的其他家庭设备设为不常用家庭
+        UpdateWrapper<FamilyUserDO> updateWrapperForUncheck = new UpdateWrapper<>();
+        updateWrapperForUncheck.set("last_checked", 0);
+        updateWrapperForUncheck.notIn("family_id", familyId);
+        updateWrapperForUncheck.eq("user_id", userId);
+        update(updateWrapperForCheck);
+    }
 
 }
