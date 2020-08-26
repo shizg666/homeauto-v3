25d0a0580d4c738144293cd13020f0dde7e32791
喻九敏 <yujiumin@landleaf-tech.com>
2020-08-26 10:04:30 +0800

    删除获取设备状态RedisKey的方法

--- homeauto-contact-screen/src/main/java/com/landleaf/homeauto/contact/screen/common/util/ContactScreenRedisKeyUtil.java
+++ homeauto-contact-screen/src/main/java/com/landleaf/homeauto/contact/screen/common/util/ContactScreenRedisKeyUtil.java
@@ -37,16 +37,4 @@ public final class ContactScreenRedisKeyUtil {
                 .append(current).toString();
     }
 
-    /**
-     * @param familyCode    家庭码
-     * @param productCode   产品码
-     * @param deviceSn      设备序列号
-     * @param attributeCode 属性码
-     * @return 设备状态的KEY值
-     * @author yujiumin
-     */
-    public static String getDeviceStatusKey(String familyCode, String productCode, String deviceSn, String attributeCode) {
-        return String.format(RedisCacheConst.FAMILY_DEVICE_STATUS_STORE_KEY, familyCode, productCode, deviceSn, attributeCode);
-    }
-
 }
