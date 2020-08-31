74c4679f3b38c4ce78ed6db5c9b45130bb5e280e
喻九敏 <yujiumin@landleaf-tech.com>
2020-08-31 17:47:22 +0800

    通过type获取终端类型

--- homeauto-common/src/main/java/com/landleaf/homeauto/common/enums/device/TerminalTypeEnum.java
+++ homeauto-common/src/main/java/com/landleaf/homeauto/common/enums/device/TerminalTypeEnum.java
@@ -3,14 +3,23 @@ package com.landleaf.homeauto.common.enums.device;
 
 /**
  * 家庭终端类型
+ *
  * @author wenyilu
  */
 public enum TerminalTypeEnum {
+
+    /**
+     * 终端:大屏
+     */
     SCREEN(1, "大屏"),
-    GATEWAY(2, "网关"),
-    ;
+
+    /**
+     * 终端:网关
+     */
+    GATEWAY(2, "网关");
 
     private Integer code;
+
     private String name;
 
     TerminalTypeEnum(Integer code, String name) {
@@ -32,5 +41,13 @@ public enum TerminalTypeEnum {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public static TerminalTypeEnum getTerminal(Integer type) {
+        if (type == 1) {
+            return SCREEN;
+        } else {
+            return GATEWAY;
+        }
     }
 }
