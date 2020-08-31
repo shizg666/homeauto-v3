cd117f5e019ff4d5301287ac8984f9b598286516
喻九敏 <yujiumin@landleaf-tech.com>
2020-08-31 17:47:46 +0800

    自由方舟APP接口交付

--- homeauto-center-device/src/main/java/com/landleaf/homeauto/center/device/controller/app/nonsmart/NonSmartDeviceController.java
+++ homeauto-center-device/src/main/java/com/landleaf/homeauto/center/device/controller/app/nonsmart/NonSmartDeviceController.java
@@ -1,23 +1,34 @@
 package com.landleaf.homeauto.center.device.controller.app.nonsmart;
 
-import com.landleaf.homeauto.center.device.model.domain.FamilyDeviceStatusDO;
-import com.landleaf.homeauto.center.device.service.mybatis.IFamilyDeviceService;
-import com.landleaf.homeauto.center.device.service.mybatis.IFamilyDeviceStatusService;
+import cn.hutool.core.collection.CollectionUtil;
+import com.landleaf.homeauto.center.device.model.bo.FamilyDeviceBO;
+import com.landleaf.homeauto.center.device.model.domain.*;
+import com.landleaf.homeauto.center.device.model.domain.category.HomeAutoProduct;
+import com.landleaf.homeauto.center.device.model.dto.DeviceCommandDTO;
+import com.landleaf.homeauto.center.device.model.vo.DeviceInfoVO;
+import com.landleaf.homeauto.center.device.model.vo.device.DevicePositionVO;
+import com.landleaf.homeauto.center.device.model.vo.device.DeviceVO;
+import com.landleaf.homeauto.center.device.service.bridge.IAppService;
+import com.landleaf.homeauto.center.device.service.mybatis.*;
 import com.landleaf.homeauto.common.domain.Response;
+import com.landleaf.homeauto.common.domain.dto.adapter.ack.AdapterDeviceControlAckDTO;
+import com.landleaf.homeauto.common.domain.dto.adapter.request.AdapterDeviceControlDTO;
+import com.landleaf.homeauto.common.enums.device.TerminalTypeEnum;
+import com.landleaf.homeauto.common.exception.BusinessException;
 import com.landleaf.homeauto.common.web.BaseController;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 /**
+ * 设备控制器
+ *
  * @author Yujiumin
  * @version 2020/8/31
  */
@@ -32,6 +43,21 @@ public class NonSmartDeviceController extends BaseController {
     @Autowired
     private IFamilyDeviceService familyDeviceService;
 
+    @Autowired
+    private IHomeAutoProductService productService;
+
+    @Autowired
+    private IHomeAutoFamilyService familyService;
+
+    @Autowired
+    private IFamilyTerminalService familyTerminalService;
+
+    @Autowired
+    private IFamilyRoomService familyRoomService;
+
+    @Autowired
+    private IAppService appService;
+
     @GetMapping("status/{deviceId}")
     @ApiOperation("获取设备状态")
     public Response<Map<String, Object>> getDeviceStatus(@PathVariable String deviceId) {
@@ -44,6 +70,54 @@ public class NonSmartDeviceController extends BaseController {
             attrMap.put(statusCode, statusValue);
         }
         return returnSuccess(attrMap);
+    }
+
+    @PostMapping("execute")
+    @ApiOperation("设备执行")
+    public Response<?> command(@RequestBody DeviceCommandDTO deviceCommandDTO) {
+        FamilyDeviceDO familyDeviceDO = familyDeviceService.getById(deviceCommandDTO.getDeviceId());
+        HomeAutoProduct product = productService.getById(familyDeviceDO.getProductId());
+        HomeAutoFamilyDO familyDO = familyService.getById(familyDeviceDO.getFamilyId());
+        FamilyTerminalDO familyTerminalDO = familyTerminalService.getById(familyDeviceDO.getTerminalId());
+        AdapterDeviceControlDTO adapterDeviceControlDTO = new AdapterDeviceControlDTO();
+        adapterDeviceControlDTO.setFamilyId(familyDeviceDO.getFamilyId());
+        adapterDeviceControlDTO.setFamilyCode(familyDO.getCode());
+        adapterDeviceControlDTO.setTerminalMac(familyTerminalDO.getMac());
+        adapterDeviceControlDTO.setTime(System.currentTimeMillis());
+        adapterDeviceControlDTO.setProductCode(product.getCode());
+        adapterDeviceControlDTO.setDeviceSn(familyDeviceDO.getSn());
+        adapterDeviceControlDTO.setData(deviceCommandDTO.getData());
+        adapterDeviceControlDTO.setTerminalType(TerminalTypeEnum.getTerminal(familyTerminalDO.getType()).getCode());
+        adapterDeviceControlDTO.setTerminalMac(familyTerminalDO.getMac());
+        AdapterDeviceControlAckDTO adapterDeviceControlAckDTO = appService.deviceWriteControl(adapterDeviceControlDTO);
+        if (Objects.equals(adapterDeviceControlAckDTO.getCode(), 200)) {
+            return returnSuccess();
+        } else {
+            throw new BusinessException(adapterDeviceControlAckDTO.getMessage());
+        }
+    }
+
+    @GetMapping("list/{familyId}")
+    @ApiOperation("获取家庭设备列表")
+    public Response<List<DevicePositionVO>> getFamilyDevices(@PathVariable String familyId) {
+        List<DevicePositionVO> devicePositionVOList = CollectionUtil.list(true);
+        List<FamilyRoomDO> roomList = familyRoomService.getRoom(familyId);
+        for (FamilyRoomDO familyRoomDO : roomList) {
+            DevicePositionVO devicePositionVO = new DevicePositionVO();
+            devicePositionVO.setRoom(familyRoomDO.getName());
+            List<FamilyDeviceDO> deviceList = familyDeviceService.getDeviceListByRoomId(familyRoomDO.getId());
+            List<DeviceVO> deviceVOList = CollectionUtil.list(true);
+            for (FamilyDeviceDO familyDeviceDO : deviceList) {
+                DeviceVO deviceVO = new DeviceVO();
+                deviceVO.setDeviceId(familyDeviceDO.getId());
+                deviceVO.setDeviceName(familyDeviceDO.getName());
+                deviceVO.setDeviceIcon(familyDeviceService.getDeviceIconById(familyDeviceDO.getId()));
+                deviceVOList.add(deviceVO);
+            }
+            devicePositionVO.setDevices(deviceVOList);
+            devicePositionVOList.add(devicePositionVO);
+        }
+        return returnSuccess(devicePositionVOList);
     }
 
 }
--- homeauto-center-device/src/main/java/com/landleaf/homeauto/center/device/controller/app/nonsmart/NonSmartFamilyController.java
+++ homeauto-center-device/src/main/java/com/landleaf/homeauto/center/device/controller/app/nonsmart/NonSmartFamilyController.java
@@ -1,12 +1,16 @@
 package com.landleaf.homeauto.center.device.controller.app.nonsmart;
 
 import cn.hutool.core.collection.CollectionUtil;
+import com.google.common.collect.Lists;
 import com.landleaf.homeauto.center.device.model.HchoEnum;
 import com.landleaf.homeauto.center.device.model.bo.DeviceSensorBO;
+import com.landleaf.homeauto.center.device.model.bo.FamilyBO;
+import com.landleaf.homeauto.center.device.model.bo.SimpleFamilyBO;
 import com.landleaf.homeauto.center.device.model.domain.FamilyDeviceDO;
 import com.landleaf.homeauto.center.device.model.domain.FamilyRoomDO;
 import com.landleaf.homeauto.center.device.model.domain.FamilySceneDO;
 import com.landleaf.homeauto.center.device.model.vo.EnvironmentVO;
+import com.landleaf.homeauto.center.device.model.vo.FamilyVO;
 import com.landleaf.homeauto.center.device.model.vo.IndexOfNonSmartVO;
 import com.landleaf.homeauto.center.device.model.vo.device.DeviceVO;
 import com.landleaf.homeauto.center.device.model.vo.scene.SceneVO;
@@ -15,6 +19,7 @@ import com.landleaf.homeauto.common.domain.Response;
 import com.landleaf.homeauto.common.web.BaseController;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -35,6 +40,9 @@ import java.util.Objects;
 public class NonSmartFamilyController extends BaseController {
 
     @Autowired
+    private IHomeAutoFamilyService familyService;
+
+    @Autowired
     private IFamilySceneService familySceneService;
 
     @Autowired
@@ -49,23 +57,62 @@ public class NonSmartFamilyController extends BaseController {
     @Autowired
     private IFamilyRoomService familyRoomService;
 
+    @GetMapping("list/{userId}")
+    @ApiOperation("获取家庭列表")
+    public Response<FamilyVO> getFamily(@PathVariable String userId) {
+        List<FamilyBO> familyBOList = familyService.getFamilyListByUserId(userId);
+        FamilyVO familyVO = new FamilyVO();
+        for (FamilyBO familyBO : familyBOList) {
+            SimpleFamilyBO family = new SimpleFamilyBO();
+            family.setFamilyId(familyBO.getFamilyId());
+            family.setFamilyName(familyBO.getFamilyName());
+            if (Objects.equals(familyBO.getLastChecked(), 1)) {
+                SimpleFamilyBO simpleFamilyBO = new SimpleFamilyBO();
+                simpleFamilyBO.setFamilyId(family.getFamilyId());
+                simpleFamilyBO.setFamilyName(family.getFamilyName());
+                familyVO.setCurrent(simpleFamilyBO);
+            }
+            if (Objects.nonNull(familyVO.getList())) {
+                familyVO.getList().add(family);
+            } else {
+                List<SimpleFamilyBO> tmpList = Lists.newArrayList();
+                SimpleFamilyBO tmpBo = new SimpleFamilyBO();
+                BeanUtils.copyProperties(family, tmpBo);
+                tmpList.add(tmpBo);
+                familyVO.setList(tmpList);
+            }
+        }
+        return returnSuccess(familyVO);
+    }
+
     @GetMapping("checkout/{familyId}")
     @ApiOperation("切换家庭")
     public Response<IndexOfNonSmartVO> getFamilyCommonScenesAndDevices(@PathVariable String familyId) {
         // 1. 获取室内环境参数
         //// 1.1 获取甲醛
         DeviceSensorBO hchoSensor = familyDeviceService.getHchoSensor(familyId);
-        String hcho = HchoEnum.getAqi(Float.parseFloat(familyDeviceService.getDeviceStatus(hchoSensor, "formaldehyde").toString()));
+        String hcho = null;
+        if (!Objects.isNull(hchoSensor)) {
+            hcho = HchoEnum.getAqi(Float.parseFloat(familyDeviceService.getDeviceStatus(hchoSensor, "formaldehyde").toString()));
+        }
 
         //// 1.2 获取pm2.5
         DeviceSensorBO pm25Sensor = familyDeviceService.getPm25Sensor(familyId);
-        String pm25Value = familyDeviceService.getDeviceStatus(pm25Sensor, "pm25").toString();
+        String pm25Value = null;
+        if (!Objects.isNull(pm25Sensor)) {
+            pm25Value = familyDeviceService.getDeviceStatus(pm25Sensor, "pm25").toString();
+        }
 
         //// 1.3 获取全/多参数传感器
         DeviceSensorBO sensor = familyDeviceService.getParamSensor(familyId);
-        String temp = familyDeviceService.getDeviceStatus(sensor, "temperature").toString();
-        String humidity = familyDeviceService.getDeviceStatus(sensor, "humidity").toString();
-        String co2 = familyDeviceService.getDeviceStatus(sensor, "co2").toString();
+        String temp = null;
+        String humidity = null;
+        String co2 = null;
+        if (!Objects.isNull(sensor)) {
+            temp = familyDeviceService.getDeviceStatus(sensor, "temperature").toString();
+            humidity = familyDeviceService.getDeviceStatus(sensor, "humidity").toString();
+            co2 = familyDeviceService.getDeviceStatus(sensor, "co2").toString();
+        }
         EnvironmentVO environmentVO = new EnvironmentVO(temp, humidity, pm25Value, hcho, co2);
 
         // 2. 获取常用场景
@@ -82,17 +129,19 @@ public class NonSmartFamilyController extends BaseController {
 
         // 3. 获取常用设备
         List<String> commonDeviceIdList = familyCommonDeviceService.getCommonDeviceIdListByFamilyId(familyId);
-        List<FamilyDeviceDO> deviceDOList = CollectionUtil.list(true, familyDeviceService.listByIds(commonDeviceIdList));
         List<DeviceVO> commonDeviceVOList = CollectionUtil.list(true);
-        for (FamilyDeviceDO familyDeviceDO : deviceDOList) {
-            commonDeviceVOList.add(toDeviceVO(familyDeviceDO));
+        if (!CollectionUtil.isEmpty(commonDeviceIdList)) {
+            List<FamilyDeviceDO> deviceDOList = CollectionUtil.list(true, familyDeviceService.listByIds(commonDeviceIdList));
+            for (FamilyDeviceDO familyDeviceDO : deviceDOList) {
+                commonDeviceVOList.add(toDeviceVO(familyDeviceDO));
+            }
         }
 
         // 4. 获取各个房间的设备
         List<FamilyRoomDO> roomList = familyRoomService.getRoom(familyId);
         Map<String, List<DeviceVO>> roomDeviceMap = CollectionUtil.newHashMap();
         for (FamilyRoomDO familyRoomDO : roomList) {
-            String position = familyRoomService.getPosition(familyRoomDO.getId());
+            String position = familyRoomService.getById(familyRoomDO.getId()).getName();
             List<FamilyDeviceDO> deviceList = familyDeviceService.getDeviceListByRoomId(familyRoomDO.getId());
             List<DeviceVO> deviceVOList = CollectionUtil.list(true);
             for (FamilyDeviceDO familyDeviceDO : deviceList) {
@@ -113,7 +162,7 @@ public class NonSmartFamilyController extends BaseController {
         DeviceVO deviceVO = new DeviceVO();
         deviceVO.setDeviceId(familyDeviceDO.getId());
         deviceVO.setDeviceName(familyDeviceDO.getName());
-        deviceVO.setPosition(familyDeviceService.getDevicePositionById(familyDeviceDO.getId()));
+        deviceVO.setPosition(familyRoomService.getById(familyDeviceDO.getRoomId()).getName());
         deviceVO.setDeviceIcon(familyDeviceService.getDeviceIconById(familyDeviceDO.getId()));
         deviceVO.setDeviceSwitch(Objects.equals(familyDeviceService.getDeviceStatus(familyDeviceDO.getId(), "switch"), "on") ? 1 : 0);
         return deviceVO;
