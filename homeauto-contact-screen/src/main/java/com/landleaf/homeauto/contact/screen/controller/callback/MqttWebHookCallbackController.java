c3953a9e4a3ade6156cb7dd835d603d1a6516e75
wenyilu <123456>
2020-09-01 13:53:39 +0800

    增加mqtt回调接口

--- homeauto-contact-screen/src/main/java/com/landleaf/homeauto/contact/screen/controller/callback/MqttWebHookCallbackController.java
+++ homeauto-contact-screen/src/main/java/com/landleaf/homeauto/contact/screen/controller/callback/MqttWebHookCallbackController.java
@@ -1,7 +1,8 @@
-package com.landleaf.homeauto.center.device.controller.callback;
+package com.landleaf.homeauto.contact.screen.controller.callback;
 
 import com.landleaf.homeauto.common.util.StreamUtils;
 import com.landleaf.homeauto.common.web.BaseController;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,15 +18,17 @@ import java.nio.charset.StandardCharsets;
  * @Version V1.0
  **/
 @RestController
-@RequestMapping("/callback/mqtt")
+@RequestMapping("/contact-screen/callback/mqtt")
+@Slf4j
 public class MqttWebHookCallbackController extends BaseController {
+
 
     @RequestMapping(value = "/web_hook", method = {RequestMethod.GET, RequestMethod.POST})
     public void webHookCallback(HttpServletRequest request) {
         try {
             byte[] body = StreamUtils.getByteByStream(request.getInputStream());
             String data = new String(body, StandardCharsets.UTF_8);
-            System.out.println(data);
+           log.info("[mqtt回调消息],{}",data);
         } catch (Exception e) {
             System.out.println("错误信息");
         }
