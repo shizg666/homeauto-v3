1cf862b508677a6c98dddfa230274a5d03702a6c
shizg <shizengguang@landleaf-tech.com>
2020-09-01 17:27:56 +0800

    app我的家庭相关

---
+++ homeauto-center-device/src/main/java/com/landleaf/homeauto/center/device/model/vo/family/FamilyTerminalVO.java
@@ -0,0 +1,46 @@
+package com.landleaf.homeauto.center.device.model.vo.family;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+import javax.validation.constraints.NotEmpty;
+
+/**
+ * <p>
+ * 家庭终端表
+ * </p>
+ *
+ * @author Yujiumin
+ * @since 2020-08-14
+ */
+@Data
+@Accessors(chain = true)
+@ApiModel(value="FamilyTerminalVO", description="家庭终端")
+public class FamilyTerminalVO {
+
+    @ApiModelProperty(value = "主键id(修改必传)")
+    private String id;
+
+    @NotEmpty(message = "项目id不能为空")
+    @ApiModelProperty(value = "项目id(必传)")
+    private String projectId;
+
+    @ApiModelProperty(value = "家庭ID")
+    private String familyId;
+
+    @NotEmpty(message = "名称不能为空")
+    @ApiModelProperty(value = "名称")
+    private String name;
+
+    @ApiModelProperty(value = "类型 1大屏2 网关")
+    private Integer type;
+
+    @ApiModelProperty(value = "是否是主网关 0否1是")
+    private Integer masterFlag;
+
+
+
+
+}
--- homeauto-center-device/src/main/java/com/landleaf/homeauto/center/device/service/mybatis/impl/FamilyTerminalServiceImpl.java
+++ homeauto-center-device/src/main/java/com/landleaf/homeauto/center/device/service/mybatis/impl/FamilyTerminalServiceImpl.java
@@ -1,12 +1,19 @@
 package com.landleaf.homeauto.center.device.service.mybatis.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.google.common.collect.Lists;
 import com.landleaf.homeauto.center.device.model.domain.FamilyTerminalDO;
+import com.landleaf.homeauto.center.device.model.domain.housetemplate.TemplateTerminalDO;
 import com.landleaf.homeauto.center.device.model.mapper.FamilyTerminalMapper;
 import com.landleaf.homeauto.center.device.model.vo.family.FamilyConfigVO;
+import com.landleaf.homeauto.center.device.model.vo.family.FamilyTerminalVO;
 import com.landleaf.homeauto.center.device.service.mybatis.IFamilyTerminalService;
+import com.landleaf.homeauto.common.constant.enums.ErrorCodeEnumConst;
+import com.landleaf.homeauto.common.domain.vo.realestate.ProjectConfigDeleteDTO;
+import com.landleaf.homeauto.common.exception.BusinessException;
+import com.landleaf.homeauto.common.util.BeanUtil;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 
@@ -29,5 +36,39 @@ public class FamilyTerminalServiceImpl extends ServiceImpl<FamilyTerminalMapper,
         queryWrapper.eq("family_id", familyId);
         queryWrapper.eq("master_flag", 1);
         return getOne(queryWrapper, true);
+    }
+
+    @Override
+    public void add(FamilyTerminalVO request) {
+        addCheck(request);
+        FamilyTerminalDO familyTerminalDO = BeanUtil.mapperBean(request,FamilyTerminalDO.class);
+        save(familyTerminalDO);
+    }
+
+    private void addCheck(FamilyTerminalVO request) {
+        int count = count(new LambdaQueryWrapper<FamilyTerminalDO>().eq(FamilyTerminalDO::getName,request.getName()).eq(FamilyTerminalDO::getFamilyId,request.getFamilyId()));
+        if (count >0){
+            throw new BusinessException(String.valueOf(ErrorCodeEnumConst.CHECK_PARAM_ERROR.getCode()), "网关名称已存在");
+        }
+    }
+
+    @Override
+    public void update(FamilyTerminalVO request) {
+        updateCheck(request);
+        FamilyTerminalDO familyTerminalDO = BeanUtil.mapperBean(request,FamilyTerminalDO.class);
+        updateById(familyTerminalDO);
+    }
+
+    private void updateCheck(FamilyTerminalVO request) {
+        FamilyTerminalDO terminalDO = getById(request.getId());
+        if (request.getName().equals(terminalDO.getName())){
+            return;
+        }
+        addCheck(request);
+    }
+
+    @Override
+    public void delete(ProjectConfigDeleteDTO request) {
+
     }
 }
