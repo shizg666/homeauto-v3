9a603c53ee3cda6f12499b6ac5c9dbd3ba84b3db
wenyilu <123456>
2020-09-01 16:54:23 +0800

    修改-app端修改密码接口

---
+++ homeauto-center-device/src/main/java/com/landleaf/homeauto/center/device/service/mybatis/IFamilyTerminalOnlineStatusService.java
@@ -0,0 +1,16 @@
+package com.landleaf.homeauto.center.device.service.mybatis;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.landleaf.homeauto.center.device.model.domain.FamilyTerminalOnlineStatusDO;
+
+/**
+ * <p>
+ * 终端在线离线状态记录表 服务类
+ * </p>
+ *
+ * @author wenyilu
+ * @since 2020-09-01
+ */
+public interface IFamilyTerminalOnlineStatusService extends IService<FamilyTerminalOnlineStatusDO> {
+
+}
