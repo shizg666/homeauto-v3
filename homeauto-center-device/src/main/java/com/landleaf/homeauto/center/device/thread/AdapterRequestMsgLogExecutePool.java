6d2511c7dfac0da264bdbd20af8e8af910672a79
wenyilu <123456>
2020-09-03 14:20:51 +0800

    增加控制命令操作日志

---
+++ homeauto-center-device/src/main/java/com/landleaf/homeauto/center/device/thread/AdapterRequestMsgLogExecutePool.java
@@ -0,0 +1,33 @@
+package com.landleaf.homeauto.center.device.thread;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
+
+/**
+ * @author wenyilu
+ */
+@Configuration
+public class AdapterRequestMsgLogExecutePool {
+    /**
+     * 记录下发给adapter 请求命令日志
+     */
+    @Bean
+    public Executor adapterRequestMsgLogExecute() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(4);
+        executor.setMaxPoolSize(8);
+        executor.setQueueCapacity(4);
+        executor.setKeepAliveSeconds(100);
+        executor.setThreadNamePrefix("AdapterRequestMsgLogExecutePool-thread");
+
+        // rejection-policy：当pool已经达到max size的时候，如何处理新任务
+        // CALLER_RUNS：不在新线程中执行任务，而是由调用者所在的线程来执行
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.DiscardPolicy());
+        executor.initialize();
+        return executor;
+    }
+}
