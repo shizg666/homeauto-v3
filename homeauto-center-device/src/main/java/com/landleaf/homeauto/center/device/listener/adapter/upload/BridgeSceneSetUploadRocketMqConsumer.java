bff2274ead26369b119726c98d1f1242c32db308
张洪滨 <zhanghongbin@landleaf-tech.com>
2020-09-03 17:26:04 +0800

    新增安防报警和场景状态上报处理

--- homeauto-center-device/src/main/java/com/landleaf/homeauto/center/device/listener/adapter/upload/BridgeSceneSetUploadRocketMqConsumer.java
+++ homeauto-center-device/src/main/java/com/landleaf/homeauto/center/device/listener/adapter/upload/BridgeSceneSetUploadRocketMqConsumer.java
@@ -4,7 +4,7 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.rocketmq.common.message.MessageExt;
 import com.landleaf.homeauto.center.device.service.bridge.BridgeUploadMessageService;
 import com.landleaf.homeauto.common.constant.RocketMqConst;
-import com.landleaf.homeauto.common.domain.dto.adapter.upload.AdapterDeviceStatusUploadDTO;
+import com.landleaf.homeauto.common.domain.dto.adapter.upload.AdapterScreenSceneSetUploadDTO;
 import com.landleaf.homeauto.common.rocketmq.consumer.RocketMQConsumeService;
 import com.landleaf.homeauto.common.rocketmq.consumer.processor.AbstractMQMsgProcessor;
 import com.landleaf.homeauto.common.rocketmq.consumer.processor.MQConsumeResult;
@@ -14,13 +14,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import java.util.List;
 
 /**
- * 收到 adapter 设备状态上报处理
+ * 收到 adapter 场景上报处理
  *
  * @author zhanghongbin
  */
-@RocketMQConsumeService(topic = RocketMqConst.TOPIC_CENTER_ADAPTER_TO_APP, tags = RocketMqConst.DEVICE_STATUS_UPLOAD)
+@RocketMQConsumeService(topic = RocketMqConst.TOPIC_CENTER_ADAPTER_TO_APP, tags = RocketMqConst.FAMILY_SECURITY_ALARM_EVENT)
 @Slf4j
-public class BridgeDeviceStatusUploadRocketMqConsumer extends AbstractMQMsgProcessor {
+public class BridgeSceneSetUploadRocketMqConsumer extends AbstractMQMsgProcessor {
 
     @Autowired
     private BridgeUploadMessageService bridgeUploadMessageService;
@@ -30,8 +30,8 @@ public class BridgeDeviceStatusUploadRocketMqConsumer extends AbstractMQMsgProce
         try {
             String msgBody = new String(message.getBody(), "utf-8");
 
-            //转换为adapter设备上报结构体
-            AdapterDeviceStatusUploadDTO uploadDTO = JSON.parseObject(msgBody, AdapterDeviceStatusUploadDTO.class);
+            //转换为adapter场景上报上报结构体
+            AdapterScreenSceneSetUploadDTO uploadDTO = JSON.parseObject(msgBody, AdapterScreenSceneSetUploadDTO.class);
 
             bridgeUploadMessageService.dealMsg(uploadDTO);
 
