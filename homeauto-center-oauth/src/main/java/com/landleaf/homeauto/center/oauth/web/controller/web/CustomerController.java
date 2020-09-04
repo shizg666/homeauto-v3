c20da88140aad61fb0145765043aad773de4f12f
wenyilu <123456>
2020-09-04 15:37:50 +0800

    修改根据项目类型查询用户

--- homeauto-center-oauth/src/main/java/com/landleaf/homeauto/center/oauth/web/controller/web/CustomerController.java
+++ homeauto-center-oauth/src/main/java/com/landleaf/homeauto/center/oauth/web/controller/web/CustomerController.java
@@ -142,9 +142,9 @@ public class CustomerController extends BaseController {
     public Response<List<CustomerSelectVO>> queryCustomerListByQuery(@RequestParam(value = "query") String query,
                                                                      @RequestParam(value = "projectType") Integer projectType) {
         String belongApp = "smart";
-        if (projectType.intValue() == 2) {
+        if (projectType.intValue() == 1) {
             belongApp = AppTypeEnum.NO_SMART.getCode();
-        } else if (projectType.intValue() == 1) {
+        } else if (projectType.intValue() == 2) {
             belongApp = AppTypeEnum.SMART.getCode();
         }
         return returnSuccess(homeAutoAppCustomerService.queryCustomerListByQuery(query, belongApp));
