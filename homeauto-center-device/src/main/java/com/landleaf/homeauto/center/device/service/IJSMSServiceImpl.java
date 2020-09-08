49d131b280a361f854d2d04a5e565c528bd494ec
张洪滨 <zhanghongbin@landleaf-tech.com>
2020-09-08 13:15:28 +0800

    极光短信调用家庭组新增用户

---
+++ homeauto-center-device/src/main/java/com/landleaf/homeauto/center/device/service/IJSMSServiceImpl.java
@@ -0,0 +1,36 @@
+package com.landleaf.homeauto.center.device.service;
+
+import com.google.common.collect.Maps;
+import com.landleaf.homeauto.center.device.bean.SmsTemplateConfig;
+import com.landleaf.homeauto.center.device.model.SmsMsgType;
+import com.landleaf.homeauto.center.device.util.JSMSUtils;
+import com.landleaf.homeauto.common.util.StringUtil;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+/**
+ * @Description 1.0
+ * @Author zhanghongbin
+ * @Date 2020/9/8 12:19
+ */
+@Service
+public class IJSMSServiceImpl implements IJSMSService{
+
+
+    @Override
+    public void groupAddUser(String projectName, String username, String mobile) {
+        SmsMsgType smsMsgType = SmsTemplateConfig.getCodeType(3);//获取新增家庭用户的模板
+
+        if (!StringUtil.isBlank(mobile)) {
+
+            Map<String, String> tempPara = Maps.newHashMap();
+            tempPara.put("projectName",projectName);
+            tempPara.put("username",username);
+            tempPara.put("mobile",mobile);
+
+            JSMSUtils.sendSmsCode(mobile, smsMsgType.getTempId(),tempPara );
+        }
+
+    }
+}
