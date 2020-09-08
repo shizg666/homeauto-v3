b8cefa73c77b1732d21ee0fec21c6b8f72a7d8d9
wenyilu <123456>
2020-09-08 16:13:23 +0800

    修改websocket

--- homeauto-center-websocket/src/main/java/com/landleaf/homeauto/center/websocket/HomeAutoCenterWebSocketApplication.java
+++ homeauto-center-websocket/src/main/java/com/landleaf/homeauto/center/websocket/HomeAutoCenterWebSocketApplication.java
@@ -2,8 +2,12 @@ package com.landleaf.homeauto.center.websocket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * WebSocket服务
@@ -13,7 +17,8 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  */
 @EnableFeignClients
 @EnableDiscoveryClient
-@SpringBootApplication
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class, MongoAutoConfiguration.class})
+@ComponentScan("com.landleaf.homeauto")
 public class HomeAutoCenterWebSocketApplication {
 
     public static void main(String[] args) {
