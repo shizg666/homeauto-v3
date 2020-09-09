5ee7e01cfd0962df41589a1dfdd36f9ccfb19d4b
喻九敏 <yujiumin@landleaf-tech.com>
2020-09-09 11:43:07 +0800

    修改日志级别

--- homeauto-center-device/src/main/java/com/landleaf/homeauto/center/device/service/mybatis/impl/FamilyDeviceStatusServiceImpl.java
+++ homeauto-center-device/src/main/java/com/landleaf/homeauto/center/device/service/mybatis/impl/FamilyDeviceStatusServiceImpl.java
@@ -50,9 +50,9 @@ public class FamilyDeviceStatusServiceImpl extends ServiceImpl<FamilyDeviceStatu
 
     @Override
     public void insertBatchDeviceStatus(List<DeviceStatusBO> deviceStatusBOList) {
-        log.debug("insertBatchDeviceStatus(List<DeviceStatusBO> deviceStatusBOList):{} ", deviceStatusBOList.toString());
+        log.info("insertBatchDeviceStatus(List<DeviceStatusBO> deviceStatusBOList):{} ", deviceStatusBOList.toString());
         for (DeviceStatusBO deviceStatusBO : deviceStatusBOList) {
-            log.debug("进入循环,deviceStatusBO的值为:{}", deviceStatusBO);
+            log.info("进入循环,deviceStatusBO的值为:{}", deviceStatusBO);
             String familyCode = deviceStatusBO.getFamilyCode();
             String productCode = deviceStatusBO.getProductCode();
             String deviceSn = deviceStatusBO.getDeviceSn();
@@ -60,10 +60,10 @@ public class FamilyDeviceStatusServiceImpl extends ServiceImpl<FamilyDeviceStatu
             String statusValue = deviceStatusBO.getStatusValue();
             String key = RedisKeyUtils.getDeviceStatusKey(familyCode, productCode, deviceSn, statusCode);
             Object deviceStatus = redisServiceForDeviceStatus.getDeviceStatus(key);
-            log.debug("上一次的状态为:{}", deviceStatus);
+            log.info("上一次的状态为:{}", deviceStatus);
             if (!Objects.isNull(deviceStatus) && Objects.equals(deviceStatus.toString(), statusValue)) {
                 // 如果设备的上次状态和上报状态一致,则更新状态的结束时间
-                log.debug("当前状态与上一次的状态一致,更新状态的结束时间");
+                log.info("当前状态与上一次的状态一致,更新状态的结束时间");
                 UpdateWrapper<FamilyDeviceStatusDO> updateWrapper = new UpdateWrapper<>();
                 updateWrapper.set("end_time", LocalDateTime.now());
                 updateWrapper.eq("family_id", familyService.getFamilyByCode(familyCode).getId());
@@ -73,7 +73,7 @@ public class FamilyDeviceStatusServiceImpl extends ServiceImpl<FamilyDeviceStatu
                 update(updateWrapper);
             } else {
                 // 如果设备的上次状态和上报状态不一致,则插入一条新的状态
-                log.debug("当前状态与上一次状态不一致,插入一条新的状态");
+                log.info("当前状态与上一次状态不一致,插入一条新的状态");
                 FamilyDeviceStatusDO familyDeviceStatusDO = new FamilyDeviceStatusDO();
                 familyDeviceStatusDO.setDeviceSn(deviceSn);
                 familyDeviceStatusDO.setStatusCode(statusCode);
