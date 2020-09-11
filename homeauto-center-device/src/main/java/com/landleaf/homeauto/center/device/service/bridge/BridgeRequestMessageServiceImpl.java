9bb1d71c255033d3cead639b488c7378fb10331b
wenyilu <123456>
2020-09-11 09:13:40 +0800

    增加必传参数校验

--- homeauto-center-device/src/main/java/com/landleaf/homeauto/center/device/service/bridge/BridgeRequestMessageServiceImpl.java
+++ homeauto-center-device/src/main/java/com/landleaf/homeauto/center/device/service/bridge/BridgeRequestMessageServiceImpl.java
@@ -4,6 +4,7 @@ import com.landleaf.homeauto.common.domain.dto.adapter.AdapterMessageBaseDTO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
 
 import javax.annotation.PostConstruct;
 import java.util.Observable;
@@ -32,10 +33,20 @@ public class BridgeRequestMessageServiceImpl extends Observable implements Bridg
 
     @Override
     public void dealMsg(AdapterMessageBaseDTO requestDTO) {
+        preCheck(requestDTO);
         // 异步通知执行
         log.info("[收到app请求控制消息]消息编码:{}",requestDTO.getMessageId());
         setChanged();
         notifyObservers(requestDTO);
+    }
+
+    private void preCheck(AdapterMessageBaseDTO requestDTO) {
+        Assert.notNull(requestDTO.getTerminalMac(),"终端mac必须");
+        Assert.notNull(requestDTO.getTerminalType(),"终端类型必须");
+        Assert.notNull(requestDTO.getFamilyCode(),"家庭编码必须");
+        Assert.notNull(requestDTO.getFamilyId(),"家庭id必须");
+        Assert.notNull(requestDTO.getMessageName(),"消息类型必须");
+        Assert.notNull(requestDTO.getMessageId(),"消息编码必须");
     }
 
 }
