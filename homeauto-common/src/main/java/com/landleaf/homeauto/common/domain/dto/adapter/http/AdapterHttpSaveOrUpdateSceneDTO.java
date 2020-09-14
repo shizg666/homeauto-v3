248e176b18d0a9ccb9cbbb6967448a68dde5f16b
wenyilu <123456>
2020-09-14 17:45:54 +0800

    增加获取场景内部通讯代码

--- homeauto-common/src/main/java/com/landleaf/homeauto/common/domain/dto/adapter/http/AdapterHttpSaveOrUpdateSceneDTO.java
+++ homeauto-common/src/main/java/com/landleaf/homeauto/common/domain/dto/adapter/http/AdapterHttpSaveOrUpdateSceneDTO.java
@@ -1,11 +1,7 @@
 package com.landleaf.homeauto.common.domain.dto.adapter.http;
 
-import com.landleaf.homeauto.common.domain.dto.adapter.AdapterMessageHttpDTO;
-import com.landleaf.homeauto.common.domain.dto.screen.http.response.ScreenHttpSceneActionDTO;
-import io.swagger.annotations.ApiModelProperty;
+import com.landleaf.homeauto.common.domain.dto.screen.http.request.ScreenHttpRequestDTO;
 import lombok.Data;
-
-import java.util.List;
 
 /**
  * @ClassName ScreenSaveOrUpdateNonSmartSceneDTO
@@ -15,34 +11,5 @@ import java.util.List;
  * @Version V1.0
  **/
 @Data
-public class AdapterHttpSaveOrUpdateSceneDTO extends AdapterMessageHttpDTO {
-
-    @ApiModelProperty(value = "场景ID")
-    private String sceneId;
-
-    @ApiModelProperty(value = "情景名称")
-    private String sceneName;
-
-    @ApiModelProperty(value = "家庭编码")
-    private String familyCode;
-
-    @ApiModelProperty(value = "场景类型1 全屋场景 2 智能场景")
-    private Integer sceneType;
-
-    @ApiModelProperty(value = "0 非默认 1 是默认")
-    private Integer defaultFlag;
-
-    @ApiModelProperty(value = "大屏是否可修改 1是 0否 ")
-    private Integer defaultFlagScreen;
-
-    @ApiModelProperty(value = "app是否可修改 1是 0否 ")
-    private Integer updateFlagApp;
-
-    @ApiModelProperty(value = "场景图标")
-    private String sceneIcon;
-
-    /**
-     * 场景动作
-     */
-    List<ScreenHttpSceneActionDTO> actions;
+public class AdapterHttpSaveOrUpdateSceneDTO extends ScreenHttpRequestDTO {
 }
