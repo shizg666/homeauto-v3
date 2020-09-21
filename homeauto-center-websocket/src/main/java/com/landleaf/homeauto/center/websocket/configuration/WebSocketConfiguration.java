682f0f5af16cf40d4c545a0d4db32188404a145a
喻九敏 <yujiumin@landleaf-tech.com>
2020-09-21 14:39:22 +0800

    订阅Redis消息

--- homeauto-center-websocket/src/main/java/com/landleaf/homeauto/center/websocket/configuration/WebSocketConfiguration.java
+++ homeauto-center-websocket/src/main/java/com/landleaf/homeauto/center/websocket/configuration/WebSocketConfiguration.java
@@ -1,14 +1,20 @@
 package com.landleaf.homeauto.center.websocket.configuration;
 
 import com.landleaf.homeauto.center.websocket.service.ConnectInterceptor;
-import com.landleaf.homeauto.center.websocket.service.MessageHandler;
+import com.landleaf.homeauto.common.constant.RedisChannelConst;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.MessageListener;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistration;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+import javax.annotation.PostConstruct;
+import java.nio.charset.StandardCharsets;
 
 /**
  * WebSocket配置
@@ -25,6 +31,18 @@ public class WebSocketConfiguration implements WebSocketConfigurer {
 
     @Autowired
     private ConnectInterceptor connectInterceptor;
+
+    @Autowired
+    private LettuceConnectionFactory lettuceConnectionFactory;
+
+    @Autowired
+    private MessageListener messageListener;
+
+    @PostConstruct
+    public void init() {
+        RedisConnection redisConnection = lettuceConnectionFactory.getConnection();
+        redisConnection.subscribe(messageListener, RedisChannelConst.WEBSOCKET_CHANNEL.getBytes(StandardCharsets.UTF_8));
+    }
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry webSocketHandlerRegistry) {
