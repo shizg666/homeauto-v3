5481aa0cff5aa1678cf13782416c15bab0a15629
喻九敏 <yujiumin@landleaf-tech.com>
2020-09-25 15:38:30 +0800

    websocket连接时校验token

--- homeauto-center-websocket/src/main/java/com/landleaf/homeauto/center/websocket/service/ConnectInterceptor.java
+++ homeauto-center-websocket/src/main/java/com/landleaf/homeauto/center/websocket/service/ConnectInterceptor.java
@@ -1,7 +1,9 @@
 package com.landleaf.homeauto.center.websocket.service;
 
 import com.landleaf.homeauto.center.websocket.feign.FamilyFeignService;
+import com.landleaf.homeauto.center.websocket.feign.GateWayRemote;
 import com.landleaf.homeauto.common.constant.CommonConst;
+import com.landleaf.homeauto.common.domain.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.server.ServerHttpRequest;
@@ -24,18 +26,23 @@ public class ConnectInterceptor implements HandshakeInterceptor {
     @Autowired
     private FamilyFeignService familyFeignService;
 
+    @Autowired
+    private GateWayRemote gateWayRemote;
+
     @Override
     public boolean beforeHandshake(ServerHttpRequest serverHttpRequest, ServerHttpResponse serverHttpResponse, WebSocketHandler webSocketHandler, Map<String, Object> map) throws Exception {
         HttpHeaders headers = serverHttpRequest.getHeaders();
         if (headers.containsKey(CommonConst.AUTHORIZATION)) {
             String authorization = headers.getFirst(CommonConst.AUTHORIZATION);
-            // TODO: 2020/9/21 验证token
-            String path = serverHttpRequest.getURI().getPath();
-            int index = path.lastIndexOf('/');
-            String familyId = path.substring(index + 1);
-            if (familyFeignService.familyExist(familyId).getResult()) {
-                map.put("familyId", familyId);
-                return true;
+            Response<Boolean> booleanResponse = gateWayRemote.checkTokenLedge(authorization);
+            if (booleanResponse.getResult()) {
+                String path = serverHttpRequest.getURI().getPath();
+                int index = path.lastIndexOf('/');
+                String familyId = path.substring(index + 1);
+                if (familyFeignService.familyExist(familyId).getResult()) {
+                    map.put("familyId", familyId);
+                    return true;
+                }
             }
         }
         return false;
