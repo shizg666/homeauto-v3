9bf64307e5d277fcbd691722eb4de96f226c870a
喻九敏 <yujiumin@landleaf-tech.com>
2020-09-25 16:19:01 +0800

    提供接口查询当前连接的家庭

--- homeauto-center-websocket/src/main/java/com/landleaf/homeauto/center/websocket/service/ConnectInterceptor.java
+++ homeauto-center-websocket/src/main/java/com/landleaf/homeauto/center/websocket/service/ConnectInterceptor.java
@@ -5,6 +5,7 @@ import com.landleaf.homeauto.center.websocket.feign.GateWayRemote;
 import com.landleaf.homeauto.common.constant.CommonConst;
 import com.landleaf.homeauto.common.domain.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
@@ -29,19 +30,27 @@ public class ConnectInterceptor implements HandshakeInterceptor {
     @Autowired
     private GateWayRemote gateWayRemote;
 
+    @Value("${debug}")
+    private boolean isDebug;
+
     @Override
     public boolean beforeHandshake(ServerHttpRequest serverHttpRequest, ServerHttpResponse serverHttpResponse, WebSocketHandler webSocketHandler, Map<String, Object> map) throws Exception {
         HttpHeaders headers = serverHttpRequest.getHeaders();
-        if (headers.containsKey(CommonConst.AUTHORIZATION)) {
-            String authorization = headers.getFirst(CommonConst.AUTHORIZATION);
-            Response<Boolean> booleanResponse = gateWayRemote.checkTokenLedge(authorization);
-            if (booleanResponse.getResult()) {
-                String path = serverHttpRequest.getURI().getPath();
-                int index = path.lastIndexOf('/');
-                String familyId = path.substring(index + 1);
-                if (familyFeignService.familyExist(familyId).getResult()) {
-                    map.put("familyId", familyId);
-                    return true;
+        String path = serverHttpRequest.getURI().getPath();
+        int index = path.lastIndexOf('/');
+        String familyId = path.substring(index + 1);
+        if (isDebug) {
+            map.put("familyId", familyId);
+            return true;
+        } else {
+            if (headers.containsKey(CommonConst.AUTHORIZATION)) {
+                String authorization = headers.getFirst(CommonConst.AUTHORIZATION);
+                Response<Boolean> booleanResponse = gateWayRemote.checkTokenLedge(authorization);
+                if (booleanResponse.getResult()) {
+                    if (familyFeignService.familyExist(familyId).getResult()) {
+                        map.put("familyId", familyId);
+                        return true;
+                    }
                 }
             }
         }
