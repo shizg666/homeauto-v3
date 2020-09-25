fcb2274de7503ac21e460135f6cc1de0da2caf2c
喻九敏 <yujiumin@landleaf-tech.com>
2020-09-25 15:54:56 +0800

    获取设备状态bug修复

--- homeauto-center-device/src/main/java/com/landleaf/homeauto/center/device/controller/app/smart/DeviceController.java
+++ homeauto-center-device/src/main/java/com/landleaf/homeauto/center/device/controller/app/smart/DeviceController.java
@@ -162,7 +162,7 @@ public class DeviceController extends BaseController {
         for (String attr : deviceBO.getDeviceAttributeList()) {
             Object deviceStatus = familyDeviceService.getDeviceStatus(deviceId, attr);
             if (Objects.isNull(deviceStatus)) {
-                deviceStatus = familyDeviceStatusService.getDeviceAttributionsById(attr);
+                deviceStatus = familyDeviceService.getDeviceStatus(deviceId, attr);
             }
             attrMap.put(attr, deviceStatus);
         }
