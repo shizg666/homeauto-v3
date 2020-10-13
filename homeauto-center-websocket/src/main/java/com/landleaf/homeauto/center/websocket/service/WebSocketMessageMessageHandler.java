f4e98048f670033b0bde30a6b586c82e439b2a1f
wenyilu <123456>
2020-10-13 14:23:59 +0800

    websocket发送改为异步

--- homeauto-center-websocket/src/main/java/com/landleaf/homeauto/center/websocket/service/WebSocketMessageMessageHandler.java
+++ homeauto-center-websocket/src/main/java/com/landleaf/homeauto/center/websocket/service/WebSocketMessageMessageHandler.java
@@ -10,12 +10,14 @@ import com.landleaf.homeauto.center.websocket.rocketmq.util.CollectionUtils;
 import com.landleaf.homeauto.common.constant.RocketMqConst;
 import com.landleaf.homeauto.common.domain.websocket.MessageModel;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.concurrent.Executor;
 
 /**
  * Redis消息监听器
@@ -27,6 +29,9 @@ import java.util.List;
 @Service
 @Consumer(group = "WEB_SOCKET", topic = RocketMqConst.TOPIC_WEBSOCKET_TO_APP)
 public class WebSocketMessageMessageHandler extends AbstractMessageHandler {
+
+    @Autowired
+    private Executor wsSendMsgExecutePool;
 
     @Override
     public ConsumeConcurrentlyStatus consumeMessage(String[] keys, String message) {
@@ -42,12 +47,18 @@ public class WebSocketMessageMessageHandler extends AbstractMessageHandler {
             for (WebSocketSession webSocketSession : webSocketSessionList) {
                 AppMessage appMessage = new AppMessage(messageModel.getMessageCode(), messageModel.getMessage());
                 String appMessageJsonString = JSON.toJSONString(appMessage);
-                try {
-                    webSocketSession.sendMessage(new TextMessage(appMessageJsonString));
-                    log.info("成功推送状态消息:{}", appMessageJsonString);
-                } catch (IOException e) {
-                    log.error("发送消息异常了,我又该肿么办....");
-                }
+
+                    wsSendMsgExecutePool.execute(new Runnable() {
+                        @Override
+                        public void run() {
+                            try {
+                            webSocketSession.sendMessage(new TextMessage(appMessageJsonString));
+                            log.info("成功推送状态消息:{}", appMessageJsonString);
+                            } catch (IOException e) {
+                                log.error("发送消息异常了,我又该肿么办....");
+                            }
+                        }
+                    });
             }
         } catch (Exception e) {
             log.error("消费消息,解析异常了,我又该肿么办....",e);
