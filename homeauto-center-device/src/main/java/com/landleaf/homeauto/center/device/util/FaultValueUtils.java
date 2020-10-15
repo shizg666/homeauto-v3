1f708168a35906454faccf4b102b41a1d0aaf688
wenyilu <123456>
2020-10-15 11:39:30 +0800

    修改故障截取

--- homeauto-center-device/src/main/java/com/landleaf/homeauto/center/device/util/FaultValueUtils.java
+++ homeauto-center-device/src/main/java/com/landleaf/homeauto/center/device/util/FaultValueUtils.java
@@ -1,10 +1,16 @@
 package com.landleaf.homeauto.center.device.util;
 
+import com.alibaba.fastjson.JSON;
+import io.netty.buffer.ByteBufUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.bouncycastle.pqc.math.linearalgebra.ByteUtils;
+
 /**
  * @Description 判断故障超过上下限的方法
  * @Author zhanghongbin
  * @Date 2020/9/7 18:06
  */
+@Slf4j
 public class FaultValueUtils {
 
     public static Integer HVAC_INT_MAX = 65536;
@@ -66,17 +72,38 @@ public class FaultValueUtils {
         }
 
         System.out.println("=======================");
+        StringBuilder sb = new StringBuilder();
 
         for (int i = 0; i < chars.length; i++) {
 
             System.out.println(chars[i]);
+            sb.append(chars[i]);
 
         }
+        log.info("转换暖通故障值为:{}", sb.toString());
         return chars;
     }
+
+    /**
+     * 将一个int数字转换为二进制的字符串形式。
+     * @param num 需要转换的int类型数据
+     * @param digits 要转换的二进制位数，位数不足则在前面补0
+     * @return 二进制的字符串形式
+     */
+    public static char[] toBinary(int num, int digits) {
+        int value = 1 << digits | num;
+        String bs = Integer.toBinaryString(value); //0x20 | 这个是为了保证这个string长度是6位数
+        String substring = bs.substring(1);
+        return substring.toCharArray();
+    }
+
     public static void main(String[] args) {
+
         System.out.println(isValueError("120","100","200"));
 
-        get16chars("255");
+        char[] chars = get16chars("256");
+        char[] chars1 = toBinary(12, 16);
+        System.out.println(chars1);
     }
+
 }
