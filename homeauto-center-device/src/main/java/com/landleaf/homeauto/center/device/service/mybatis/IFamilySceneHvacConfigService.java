b7c81bf6246026f1c68b9203992715a19d4f51d0
喻九敏 <yujiumin@landleaf-tech.com>
2020-11-05 11:51:34 +0800

    修改逻辑: 全屋场景详情里展示暖通设备

--- homeauto-center-device/src/main/java/com/landleaf/homeauto/center/device/service/mybatis/IFamilySceneHvacConfigService.java
+++ homeauto-center-device/src/main/java/com/landleaf/homeauto/center/device/service/mybatis/IFamilySceneHvacConfigService.java
@@ -13,9 +13,18 @@ public interface IFamilySceneHvacConfigService extends IService<FamilySceneHvacC
 
     /**
      * 根据设备号和家庭id查询改设备关联的场景暖通配置id集合
+     *
      * @param deviceSn
      * @param familyId
      * @return
      */
     List<String> getListIds(String deviceSn, String familyId);
+
+    /**
+     * 通过 sceneId 查询实体列表
+     *
+     * @param sceneId
+     * @return
+     */
+    List<FamilySceneHvacConfig> listBySceneId(String sceneId);
 }
