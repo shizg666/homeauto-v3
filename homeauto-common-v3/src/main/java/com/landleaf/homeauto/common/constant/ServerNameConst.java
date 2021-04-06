e153934d0644396f20cb5c7f81392ee7b32976b0
shizg <shizengguang@landleaf-tech.com>
2021-04-06 14:03:04 +0800

    修改

--- homeauto-common-v3/src/main/java/com/landleaf/homeauto/common/constant/ServerNameConst.java
+++ homeauto-common-v3/src/main/java/com/landleaf/homeauto/common/constant/ServerNameConst.java
@@ -10,52 +10,52 @@ public interface ServerNameConst {
     /**
      * id服务
      */
-    String HOMEAUTO_CENTER_ID = "homeauto-center-id";
+    String HOMEAUTO_CENTER_ID = "homeauto-center-id-v3";
 
     /**
      * 用户服务
      */
-    String HOMEAUTO_CENTER_OAUTH = "homeauto-center-oauth";
+    String HOMEAUTO_CENTER_OAUTH = "homeauto-center-oauth-v3";
 
     /**
      * 路由服务
      */
-    String HOMEAUTO_CENTER_GATEWAY = "homeauto-center-gateway";
+    String HOMEAUTO_CENTER_GATEWAY = "homeauto-center-gateway-v3";
 
     /**
      * 中心服务
      */
-    String HOMEAUTO_CENTER_DEVICE = "homeauto-center-device";
+    String HOMEAUTO_CENTER_DEVICE = "homeauto-center-device-v3";
 
     /**
      * 文件服务
      */
-    String HOMEAUTO_CENTER_FILE = "homeauto-center-file";
+    String HOMEAUTO_CENTER_FILE = "homeauto-center-file-v3";
 
     /**
      * 通信适配器
      */
-    String HOMEAUTO_CENTER_ADAPTER = "homeauto-center-adapter";
+    String HOMEAUTO_CENTER_ADAPTER = "homeauto-center-adapter-v3";
 
     /**
      * 天气服务
      */
-    String HOMEAUTO_CENTER_WEATHER = "homeauto-center-weather";
+    String HOMEAUTO_CENTER_WEATHER = "homeauto-center-weather-v3";
 
     /**
      * WebSocket服务
      */
-    String HOMEAUTO_CENTER_WEBSOCKET = "homeauto-center-websocket";
+    String HOMEAUTO_CENTER_WEBSOCKET = "homeauto-center-websocket-v3";
 
     /**
      * 网关通信服务
      */
-    String HOMEAUTO_CONTACT_GATEWAY = "homeauto-contact-gateway";
+    String HOMEAUTO_CONTACT_GATEWAY = "homeauto-contact-gateway-v3";
 
     /**
      * 大屏网关通信服务
      */
-    String HOMEAUTO_CONTACT_SCREEN = "homeauto-contact-screen";
+    String HOMEAUTO_CONTACT_SCREEN = "homeauto-contact-screen-v3";
 
 
 }
