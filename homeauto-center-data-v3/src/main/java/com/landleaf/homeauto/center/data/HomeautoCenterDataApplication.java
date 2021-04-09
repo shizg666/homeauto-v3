db0269198c479d15567c9848399b4443b07dba8e
温奕禄 <wenyilu@landleaf-tech.com>
2021-04-09 10:24:22 +0800

    data主键生成订阅

--- homeauto-center-data-v3/src/main/java/com/landleaf/homeauto/center/data/HomeautoCenterDataApplication.java
+++ homeauto-center-data-v3/src/main/java/com/landleaf/homeauto/center/data/HomeautoCenterDataApplication.java
@@ -3,11 +3,15 @@ package com.landleaf.homeauto.center.data;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan("com.landleaf.homeauto.*")
 @MapperScan("com/landleaf/homeauto/**/mapper")
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients("com.landleaf.homeauto.center.data.remote")
 public class HomeautoCenterDataApplication {
 
     public static void main(String[] args) {
