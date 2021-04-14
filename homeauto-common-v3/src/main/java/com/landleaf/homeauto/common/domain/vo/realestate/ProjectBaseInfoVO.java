673465992adbf57fd3eeba5fdb64741dcdc129fd
shizg <shizengguang@landleaf-tech.com>
2021-04-14 16:40:44 +0800

    修改

--- homeauto-common-v3/src/main/java/com/landleaf/homeauto/common/domain/vo/realestate/ProjectBaseInfoVO.java
+++ homeauto-common-v3/src/main/java/com/landleaf/homeauto/common/domain/vo/realestate/ProjectBaseInfoVO.java
@@ -1,5 +1,6 @@
 package com.landleaf.homeauto.common.domain.vo.realestate;
 
+import com.landleaf.homeauto.common.enums.realestate.ProjectStatusEnum;
 import com.landleaf.homeauto.common.enums.realestate.ProjectTypeEnum;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
@@ -27,11 +28,23 @@ public class ProjectBaseInfoVO {
     @ApiModelProperty(value = "类型Str")
     private String typeSr;
 
+    @ApiModelProperty(value = "状态")
+    private Integer status;
+
+    @ApiModelProperty(value = "状态Str")
+    private String statusStr;
+
+
     @ApiModelProperty(value = "名称")
     private String name;
 
     public void setType(Integer type) {
         this.type = type;
         this.typeSr = ProjectTypeEnum.getInstByType(type) != null?ProjectTypeEnum.getInstByType(type).getName():"";
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+        this.statusStr = ProjectStatusEnum.getInstByType(status) != null?ProjectTypeEnum.getInstByType(status).getName():"";
     }
 }
