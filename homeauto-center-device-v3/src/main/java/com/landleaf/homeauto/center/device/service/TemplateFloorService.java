825a072ede9696dba3fee73d031dfe2f67de233e
wenyilu <123456>
2021-04-20 10:26:29 +0800

    修改设备图片获取

--- homeauto-center-device-v3/src/main/java/com/landleaf/homeauto/center/device/service/TemplateFloorService.java
+++ homeauto-center-device-v3/src/main/java/com/landleaf/homeauto/center/device/service/TemplateFloorService.java
@@ -2,12 +2,14 @@ package com.landleaf.homeauto.center.device.service;
 
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
+import com.landleaf.homeauto.center.device.model.domain.category.HomeAutoProduct;
 import com.landleaf.homeauto.center.device.model.domain.housetemplate.TemplateDeviceDO;
 import com.landleaf.homeauto.center.device.model.domain.housetemplate.TemplateFloorDO;
 import com.landleaf.homeauto.center.device.model.domain.housetemplate.TemplateRoomDO;
 import com.landleaf.homeauto.center.device.model.vo.DeviceInfoVO;
 import com.landleaf.homeauto.center.device.model.vo.FloorRoomVO;
 import com.landleaf.homeauto.center.device.model.vo.RoomDeviceVO;
+import com.landleaf.homeauto.center.device.service.mybatis.IHomeAutoProductService;
 import com.landleaf.homeauto.center.device.service.mybatis.IHouseTemplateDeviceService;
 import com.landleaf.homeauto.center.device.service.mybatis.IHouseTemplateRoomService;
 import com.landleaf.homeauto.common.util.BeanUtil;
@@ -15,6 +17,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -32,19 +35,27 @@ public class TemplateFloorService implements ITemplateFloorService {
     private IHouseTemplateRoomService templateRoomService;
     @Autowired
     private IHouseTemplateDeviceService templateDeviceService;
+    @Autowired
+    private IHomeAutoProductService productService;
 
     @Override
     public List<FloorRoomVO> getFloorAndRoomDevices(String templateId, Integer deviceShowApp) {
         List<FloorRoomVO> result = Lists.newArrayList();
         List<TemplateRoomDO> rooms = templateRoomService.getRoomsByTemplateId(Long.parseLong(templateId));
         List<TemplateDeviceDO> devices = templateDeviceService.listByTemplateId(Long.parseLong(templateId));
+
         Map<String, List<TemplateDeviceDO>> ROOM_MAP = Maps.newHashMap();
+        Map<Long, HomeAutoProduct> PRODUCT_MAP = Maps.newHashMap();
         if (!CollectionUtils.isEmpty(devices)) {
             ROOM_MAP = devices.stream().collect(Collectors.groupingBy(i->{return String.valueOf(i.getId());}));
+            PRODUCT_MAP = productService.listByIds(devices.stream().map(i -> {
+                return i.getProductId();
+            }).collect(Collectors.toList())).stream().collect(Collectors.toMap(p -> p.getId(), p -> p, (o, n) -> n));
         }
         if (!CollectionUtils.isEmpty(rooms)) {
             Map<String, List<TemplateRoomDO>> FLOOR_MAP = rooms.stream().collect(Collectors.groupingBy(TemplateRoomDO::getFloor));
             Map<String, List<TemplateDeviceDO>> finalROOM_MAP = ROOM_MAP;
+            Map<Long, HomeAutoProduct> finalPRODUCT_MAP = PRODUCT_MAP;
             FLOOR_MAP.forEach((k, v) -> {
                 FloorRoomVO floorRoomVO = new FloorRoomVO();
                 floorRoomVO.setFloor(k);
@@ -58,7 +69,7 @@ public class TemplateFloorService implements ITemplateFloorService {
                         roomDeviceVO.setDevices(templateDeviceDOS.stream().map(d -> {
                             DeviceInfoVO deviceInfoVO = new DeviceInfoVO();
                             deviceInfoVO.setId(String.valueOf(d.getId()));
-                            deviceInfoVO.setIcon(d.getImageIcon());
+                            deviceInfoVO.setIcon(finalPRODUCT_MAP.get(d.getProductId()).getIcon2());
                             return deviceInfoVO;
                         }).collect(Collectors.toList()));
                     }
