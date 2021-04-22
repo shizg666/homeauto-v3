8c3203bd0b257c4792b9173347b20ac0a96ebd23
shizg <shizengguang@landleaf-tech.com>
2021-04-22 14:13:20 +0800

    修改

--- homeauto-center-device-v3/src/main/java/com/landleaf/homeauto/center/device/model/vo/scene/SceneAttributeInfoBO.java
+++ homeauto-center-device-v3/src/main/java/com/landleaf/homeauto/center/device/model/vo/scene/SceneAttributeInfoBO.java
@@ -2,6 +2,7 @@ package com.landleaf.homeauto.center.device.model.vo.scene;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import jdk.nashorn.internal.ir.annotations.Ignore;
 import lombok.Data;
 
 /**
@@ -12,9 +13,11 @@ import lombok.Data;
 @ApiModel(value="SceneAttributeInfoBO", description="SceneAttributeInfoBO")
 public class SceneAttributeInfoBO {
 
+    @ApiModelProperty("属性code")
+    @Ignore
+    private Long id;
 
-
-    @ApiModelProperty("属性id")
+    @ApiModelProperty("属性code")
     private String attributeCode;
 
     @ApiModelProperty("属性当前值")
