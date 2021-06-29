7ecee10a4b2a52e75e869520a2c4efe13f8a94e2
wenyilu <123456>
2021-06-29 10:14:03 +0800

    增加白名單

--- homeauto-center-device-v3/src/main/java/com/landleaf/homeauto/center/device/service/mybatis/impl/HomeAutoFamilyServiceImpl.java
+++ homeauto-center-device-v3/src/main/java/com/landleaf/homeauto/center/device/service/mybatis/impl/HomeAutoFamilyServiceImpl.java
@@ -944,8 +944,6 @@ public class HomeAutoFamilyServiceImpl extends ServiceImpl<HomeAutoFamilyMapper,
                 StringUtils.isEmpty(unitCode) ||
                 StringUtils.isEmpty(floor) ||
                 StringUtils.isEmpty(roomNo) ||
-                StringUtils.isEmpty(prefix) ||
-                StringUtils.isEmpty(suffix) ||
                 StringUtils.isEmpty(terminalMac)
         ) {
             throw new BusinessException("缺少必要参数!");
@@ -959,8 +957,12 @@ public class HomeAutoFamilyServiceImpl extends ServiceImpl<HomeAutoFamilyMapper,
         queryWrapper2.eq("unit_code", unitCode);
         queryWrapper2.eq("floor", floor);
         queryWrapper2.eq("room_no", roomNo);
-        queryWrapper2.eq("prefix", prefix);
-        queryWrapper2.eq("suffix", suffix);
+        if(!StringUtils.isEmpty(prefix)){
+            queryWrapper2.eq("prefix", prefix);
+        }
+        if(!StringUtils.isEmpty(suffix)){
+            queryWrapper2.eq("suffix", suffix);
+        }
 
         HomeAutoFamilyDO exits = getOne(queryWrapper1);
         HomeAutoFamilyDO updateFamily = getOne(queryWrapper2);
